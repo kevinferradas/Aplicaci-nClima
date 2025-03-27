c3d319c54bceda11fb883bf307dd5a984ca90083
kevin <kferradas@unitru.edu.pe>
2025-03-27 18:47:54 +0100

    Aplicacion 27/03/2025

---
+++ js/app.js
@@ -0,0 +1,20 @@
+// https://api.openweathermap.org/data/2.5/weather?q=Barcelona&appid=1478ad26c8d8533a468a8f1b34d91a65&units=metric&lang=es
+
+import key from './key.js'
+
+const appid = "1478ad26c8d8533a468a8f1b34d91a65"
+const ciudad ="Barcelona"
+const idioma="es"
+let url = `https://api.openweathermap.org/data/2.5/weather?appid=${key}&units=metric&lang=${idioma}&q=${ciudad}`
+
+
+const divDatos=document.getElementById("divDatos")
+
+fetch(url)
+.then(data => data.json())
+.then(data => {
+    divDatos.innerHTML +=`<p>Ciudad: ${data.name}</p>`
+    divDatos.innerHTML +=`<p>Temperatura actual: ${data['main']['temp']}ºC</p>`
+    divDatos.innerHTML += `<p>Descripción: ${data['weather'][0]['description']}</p>`
+    divDatos.innerHTML += `<div><img src= "https://www.imelcf.gob.pa/wp-content/plugins/location-weather/assets/images/icons/weather-icons/${data['weather'][0]['icon']}.svg"></div>`
+})

b38609eea196bfa884b9ea6f6edbe6cf1613b4cd
Kevin <kferradas@unitru.edu.pe>
2025-03-28 14:15:30 +0100

    avance app 28/03/2025

--- js/app.js
+++ js/app.js
@@ -2,9 +2,6 @@
 import key from './key.js'
 
 
-
-
-
 const formClima =document.forms["formClima"]
 
 formClima.addEventListener("submit", (e) => {
@@ -13,8 +10,10 @@ formClima.addEventListener("submit", (e) => {
 
     let ciudad = formClima["ciudad"].value
     let idioma =  formClima["idioma"].value
-    let url = `https://api.openweathermap.org/data/2.5/weather?appid=${key}&units=metric&lang=${idioma}&q=${ciudad}`
+    // let url = `https://api.openweathermap.org/data/2.5/weather?appid=${key}&units=metric&lang=${idioma}&q=${ciudad}`
+    let url = `https://api.openweathermap.org/data/2.5/forecast?appid=${key}&units=metric&lang=${idioma}&q=${ciudad}`
 
+ alert(`https://api.openweathermap.org/data/2.5/forecast?appid=${key}&units=metric&lang=${idioma}&q=${ciudad}`)
 
     const divDatos=document.getElementById("divDatos")
 
@@ -23,7 +22,8 @@ formClima.addEventListener("submit", (e) => {
     fetch(url)
     .then(data => data.json())
     .then(data => {
-    divDatos.innerHTML +=`<p> <span id="parametros"> Ciudad </span>: ${data.name}</p>`
+    // divDatos.innerHTML +=`<p> <span id="parametros"> Ciudad </span>: ${data.name}</p>`
+    divDatos.innerHTML +=`<p> <span id="parametros"> Ciudad </span>: ${data["city"]["name"]}</p>`
     divDatos.innerHTML +=`<p><span id="parametros"> Temperatura actual </span>: ${data['main']['temp']}ºC</p>`
     divDatos.innerHTML += `<p><span id="parametros"> Descripción </span> : ${data['weather'][0]['description']}</p>`
     divDatos.innerHTML += `<div><img src= "https://www.imelcf.gob.pa/wp-content/plugins/location-weather/assets/images/icons/weather-icons/${data['weather'][0]['icon']}.svg"></div>`
@@ -31,5 +31,7 @@ formClima.addEventListener("submit", (e) => {
        
     
 })
+
+
 
 

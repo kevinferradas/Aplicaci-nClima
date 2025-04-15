3aa628d9e9abd42c6b248c897b895dfcfcd1605a
Kevin <kferradas@unitru.edu.pe>
2025-04-15 13:12:33 +0200

    aplicación finalizada-falta responsividad

--- js/app.js
+++ js/app.js
@@ -24,7 +24,7 @@ formClima.addEventListener("submit", (e) => {
     // URL de la API
     let url = `https://api.openweathermap.org/data/2.5/forecast?appid=${key}&units=metric&lang=${idioma}&q=${ciudad}`;
 
-    alert (url)
+    // alert (url)
     // Limpiamos las previsiones anteriores
     divDatos.innerHTML = "";
     sectionClimaDia.style.display = "none";  
@@ -36,31 +36,31 @@ formClima.addEventListener("submit", (e) => {
         .then(data => {
             let mensajeClima = `
                 <div class="clima-container">
-                    <h2 class="tituloClima">El tiempo en ${ciudad}, hoy</h2>
+                    <h2 class="tituloClima">EL TIEMPO EN <span class="parametros">${ciudad.toUpperCase()}</span>, HOY : </h2>
                     <div class="clima-flex">
                         <!-- Columna Izquierda -->
                         <div class="columna-izquierda">
                             <div id="sensacionTermica">
-                                <p><i class="fa-solid fa-temperature-half"></i> <span id="parametros">Sensación térmica</span></p>
+                                <p><i class="fa-solid fa-temperature-half" ></i> <span class="parametros">Sensación térmica</span></p>
                                 <p class="valor-destacado">${data["list"][0]['main']["feels_like"]} ºC</p>
                             </div>
                             <div class="param-col">
-                                <p><i class="fa-solid fa-arrow-up"></i> <span id="parametros">Máx.</span> / <i class="fa-solid fa-arrow-down"></i> <span id="parametros"> Mín.</span>: ${data["list"][0]['main']["temp_max"]}º / ${data["list"][0]['main']['temp_min']}º</p>
-                                <p><i class="fa-solid fa-droplet"></i> <span id="parametros">Humedad</span>: ${data["list"][0]['main']["humidity"]} %</p>
-                                <p><i class="fa-solid fa-eye"></i> <span id="parametros">Visibilidad</span>: ${data["list"][0]['visibility']} km</p>
+                                <p><i class="fa-solid fa-arrow-up" ></i> <span class="parametros">Máx.</span> / <i class="fa-solid fa-arrow-down" ></i> <span class="parametros"> Mín.</span>: ${data["list"][0]['main']["temp_max"]}º / ${data["list"][0]['main']['temp_min']}º</p>
+                                <p><i class="fa-solid fa-droplet" ></i> <span class="parametros">Humedad</span>: ${data["list"][0]['main']["humidity"]} %</p>
+                                <p><i class="fa-solid fa-eye" ></i> <span class="parametros">Visibilidad</span>: ${data["list"][0]['visibility']} m</p>
                             </div>
                         </div>
 
                         <!-- Columna Derecha -->
                         <div class="columna-derecha">
                             <div id="descripcion">
-                                <p> <span id="parametros">Descripción</span>: ${data['list'][0]["weather"][0]['description']}</p>
+                                <p> <span class="parametros">Descripción</span>: ${data['list'][0]["weather"][0]['description']}</p>
                                 <img src="https://www.imelcf.gob.pa/wp-content/plugins/location-weather/assets/images/icons/weather-icons/${data['list'][0]["weather"][0]['icon']}.svg" alt="Icono clima">
                             </div>
                             <div class="param-col">
-                                <p><i class="fa-solid fa-wind"></i> <span id="parametros">Viento</span>: ${data["list"][0]['wind']["speed"]} m/s</p>
-                                <p> <i class="fa-solid fa-cloud"></i> <span id="parametros">Nubosidad</span>: ${data["list"][0]['clouds']["all"]} %</p>
-                                <p><i class="fa-solid fa-cloud-rain"></i> <span id="parametros">Precipitación</span>: ${data["list"][0]['pop']} %</p>
+                                <p><i class="fa-solid fa-wind" ></i> <span class="parametros">Viento</span>: ${data["list"][0]['wind']["speed"]} m/s</p>
+                                <p> <i class="fa-solid fa-cloud" ></i> <span class="parametros">Nubosidad</span>: ${data["list"][0]['clouds']["all"]} %</p>
+                                <p><i class="fa-solid fa-cloud-rain" ></i> <span class="parametros">Precipitación</span>: ${data["list"][0]['pop']} %</p>
                             </div>
                         </div>
                     </div>
@@ -73,37 +73,106 @@ formClima.addEventListener("submit", (e) => {
     }
     // Rango por horas
     else if (rango == "2") {
-        let listaClima = [];
+        let listaClima = []
+        divDatos.style.display = "none";
+
+        
         fetch(url)
         .then(data => data.json())
         .then(data => {
+            let mensajeClima1 =   ` 
+                                    <h2 class="tituloClima">EL TIEMPO EN <span class="parametros">${ciudad.toUpperCase()}</span>: </h2>
+                                    `
+  
             data["list"].forEach(element => {
                 let hora = element["dt_txt"].split(' ')[1].slice(0,5);
                 if (element["dt_txt"].split(' ')[0] == hoy) {
-                    let mensajeClima = `
+                let   mensajeClima2 = `
+
                         <div class ="climaPorhoras">
-                            <div><p><span id="parametros">Hora</span>: ${hora}</p></div>
-                            <div><p><span id="parametros">Ciudad</span>: ${data["city"]["name"]}</p></div>
-                            <div><p><span id="parametros">Temperatura</span>: ${element['main']['temp']} ºC</p></div>
-                            <div><p><span id="parametros">Descripción</span>: ${element["weather"][0]['description']}</p></div>
-                            <div><p><span id="parametros">Precipitación</span>: ${element['pop']} %</p></div>
-                            <div><p><span id="parametros">Viento</span>: ${data["list"][0]['wind']["speed"]} m/s</p></div>
+                            <div>
+                                <p class = "hora">${hora}</p>
+                                <p><i class="fa-solid fa-temperature-half"></i><span>${element['main']["feels_like"]} ºC </span></p>
+                            </div>
+                            <div id = "descripcion_horas">
+                                <img src="https://www.imelcf.gob.pa/wp-content/plugins/location-weather/assets/images/icons/weather-icons/${element["weather"][0]['icon']}.svg" alt="Icono clima">
+                                <p> ${element["weather"][0]['description']}</p>
+                            </div>
+                            <div>
+                            <p><i class="fa-solid fa-cloud-rain"></i> ${element['pop']} %</p>
+                            <p><i class="fa-solid fa-wind"></i> ${element['wind']["speed"]} m/s</p>
+                            </div>
+                            <button class="btn-toggle">+</button>
                         </div>
-                    `;
-                    listaClima.push(mensajeClima);
+
+                        <div class="climaDetalle" style="display: none;">
+                            <div class="divDetalle" >
+                                <div>
+                                <p><i class="fa-solid fa-arrow-up" ></i> <span class="parametros">Máx.</span> / <i class="fa-solid fa-arrow-down" ></i> <span class="parametros"> Mín.</span>: ${element.main.temp_max}º / ${element.main.temp_min}º</p>
+                                </div>
+                                <div>
+                                <p><i class="fa-solid fa-droplet"></i> Humedad: ${element.main.humidity}%</p>
+                                </div>
+                            </div>
+                            <div class="divDetalle" >
+                                <div>
+                                <p> <i class="fa-solid fa-cloud" ></i> <span class="parametros">Nubosidad</span>: ${element['clouds']["all"]} %</p>
+                                </div>
+                                <div>
+                                <p><i class="fa-solid fa-eye"></i> Visibilidad: ${element.visibility} m</p>
+                                </div>
+                        
+                            </div>
+                        </div>
+                        
+                    `
+                    ;
+                    listaClima.push(mensajeClima2);
                 }
             });
 
-            listaClima.forEach(mensajes => {
-                divDatos.innerHTML += mensajes;
-            });
+            listaClima.unshift(mensajeClima1)
 
-            divDatos.style.display = "flex";
+            listaClima.forEach(mensajes => {
+                
+                divDatos.innerHTML += mensajes;
+                
+            });
+            divDatos.style.display = "block";
+
+            // Activar botones +/-
+
+    let botones = document.querySelectorAll(".btn-toggle");
+let climaDetalle = document.querySelectorAll(".climaDetalle");
+
+botones.forEach((btn, index) => {
+    btn.addEventListener("click", () => {
+        const detalle = climaDetalle[index];
+        const isVisible = detalle.style.display === "block";
+
+        // Ocultar todos los detalles y resetear botones
+        climaDetalle.forEach((d, i) => {
+            d.style.display = "none";
+            botones[i].textContent = "+";
+        });
+
+        // Si el que clickeaste no estaba visible, mostrarlo
+        if (!isVisible) {
+            detalle.style.display = "block";
+            btn.textContent = "-";
+        }
+    });
+});
+
+
+
+
         });
     }
     // Rango por días
     else {
         let listaDias = [];
+        divDatos.style.display = "none"
         divDias.innerHTML = "";
         fetch(url)
         .then(data => data.json())
@@ -124,31 +193,89 @@ formClima.addEventListener("submit", (e) => {
             climaPordias.addEventListener('submit', (e) => {
                 e.preventDefault();
                 divDatos.innerHTML = "";
-                let listaClimadia = [];
+                let listaClimaDia = []
+                let mensajeClima1 =   ` 
+                                    <h2 class="tituloClima">EL TIEMPO EN <span class="parametros">${ciudad.toUpperCase()}</span>: </h2>
+                                    `
                 data["list"].forEach(element => {
                     let diaElegido = climaPordias["diaClima"].value;
                     let hora = element["dt_txt"].split(' ')[1].slice(0, 5);
                     if (element["dt_txt"].split(' ')[0] == diaElegido) {
-                        let mensajeClima = `
-                            <div class ="climaPorhoras">
-                                <div><p><span id="parametros">Día</span>: ${diaElegido}</p></div>
-                                <div><p><span id="parametros">Hora</span>: ${hora}</p></div>
-                                <div><p><span id="parametros">Ciudad</span>: ${data["city"]["name"]}</p></div>
-                                <div><p><span id="parametros">Temperatura</span>: ${element['main']['temp']} ºC</p></div>
-                                <div><p><span id="parametros">Descripción</span>: ${element["weather"][0]['description']}</p></div>
-                                <div><p><span id="parametros">Precipitación</span>: ${element['pop']} %</p></div>
-                                <div><p><span id="parametros">Viento</span>: ${data["list"][0]['wind']["speed"]} m/s</p></div>
+
+                        let   mensajeClima2 = `
+
+                        <div class ="climaPorhoras">
+                            <div>
+                                <p>${hora}</p>
+                                <p><i class="fa-solid fa-temperature-half"></i><span>${element['main']["feels_like"]} ºC </span></p>
                             </div>
-                        `;
-                        listaClimadia.push(mensajeClima);
+                            <div id = "descripcion_horas">
+                                <img src="https://www.imelcf.gob.pa/wp-content/plugins/location-weather/assets/images/icons/weather-icons/${element["weather"][0]['icon']}.svg" alt="Icono clima">
+                                <p> ${element["weather"][0]['description']}</p>
+                            </div>
+                            <div>
+                            <p><i class="fa-solid fa-cloud-rain"></i> ${element['pop']} %</p>
+                            <p><i class="fa-solid fa-wind"></i> ${element['wind']["speed"]} m/s</p>
+                            </div>
+                            <button class="btn-toggle">+</button>
+                        </div>
+                        <div class="climaDetalle" style="display: none;">
+                            <div class="divDetalle" >
+                                <div>
+                                <p><i class="fa-solid fa-arrow-up" ></i> <span class="parametros">Máx.</span> / <i class="fa-solid fa-arrow-down" ></i> <span class="parametros"> Mín.</span>: ${element.main.temp_max}º / ${element.main.temp_min}º</p>
+                                </div>
+                                <div>
+                                <p><i class="fa-solid fa-droplet"></i> Humedad: ${element.main.humidity}%</p>
+                                </div>
+                            </div>
+                            <div class="divDetalle" >
+                                <div>
+                                <p> <i class="fa-solid fa-cloud" ></i> <span class="parametros">Nubosidad</span>: ${element['clouds']["all"]} %</p>
+                                </div>
+                                <div>
+                                <p><i class="fa-solid fa-eye"></i> Visibilidad: ${element.visibility} m</p>
+                                </div>
+                        
+                            </div>
+                        </div>
+                        
+                    `
+                    ;
+                    listaClimaDia.push(mensajeClima2);
                     }
                 });
 
-                listaClimadia.forEach(mensajes => {
-                    divDatos.innerHTML += mensajes;
-                });
+                listaClimaDia.unshift(mensajeClima1)
 
-                divDatos.style.display = "flex";
+                listaClimaDia.forEach(mensajes => {
+                
+                divDatos.innerHTML += mensajes;
+                
+            });
+            divDatos.style.display = "block";
+
+            let botones = document.querySelectorAll(".btn-toggle");
+            let climaDetalle = document.querySelectorAll(".climaDetalle");
+            
+            botones.forEach((btn, index) => {
+                btn.addEventListener("click", () => {
+                    const detalle = climaDetalle[index];
+                    const isVisible = detalle.style.display === "block";
+            
+                    // Ocultar todos los detalles y resetear botones
+                    climaDetalle.forEach((d, i) => {
+                        d.style.display = "none";
+                        botones[i].textContent = "+";
+                    });
+            
+                    // Si el que clickeaste no estaba visible, mostrarlo
+                    if (!isVisible) {
+                        detalle.style.display = "block";
+                        btn.textContent = "-";
+                    }
+                });
+            });
+           
             });
         });
     }
